8684ad565ebc326e247ce197b4b57fb46bad065b
Helmut Januschka <h.januschka@krone.at>
2017-02-09 10:09:26 +0100

    s

--- spec/google_analytics_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::GoogleAnalyticsAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The google_analytics plugin is working!")
-
-      Fastlane::Actions::GoogleAnalyticsAction.run(nil)
-    end
-  end
-end
